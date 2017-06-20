7b2aee1c12432ba3a0536a71eba87cb8e8ef5df4
LYNX - Akif Vohra <a.vohra@lynxbroker.de>
2017-06-20 20:40:49 +0200

    Multi loop fixed

--- lms/djangoapps/customizations/management/commands/sales_email.py
+++ lms/djangoapps/customizations/management/commands/sales_email.py
@@ -24,6 +24,7 @@ from customizations.utils import send_sales_email, add_user_to_mailchimp, add_us
 import logging
 import os
 
+
 class Command(BaseCommand):
     """
     Import the specified data directory into the default ModuleStore
@@ -43,9 +44,8 @@ class Command(BaseCommand):
         except InvalidKeyError:
             course_key = SlashSeparatedCourseKey.from_deprecated_string('course-v1:LYNX+001+2016')
 
-
         course = get_course(course_key)
-        
+
         enrollments = CourseEnrollment.objects.filter(course_id=course.id, is_active=True)
         enrolled_students = CourseEnrollment.objects.users_enrolled_in(course_id=course.id)
 
@@ -56,42 +56,43 @@ class Command(BaseCommand):
 
         for student, gradeset, err_msg in iterate_grades_for(course.id, enrolled_students):
             if gradeset:
-                for enrollment in enrollments:
-                    # First check for welcome email
-                    sales_email_history, is_new = SalesEmailHistory.objects.get_or_create(enrollment=enrollment)
+                enrollment = enrollments.get(user=student)
+                # First check for welcome email
+                sales_email_history, is_new = SalesEmailHistory.objects.get_or_create(enrollment=enrollment)
 
-                    # Get User Enrollments
-                    user = enrollment.user
-                    
-                    enrolled_at = enrollment.created
-                    
-                    time_diff = (timezone.now() - enrolled_at).days
-                    percent = gradeset['percent']
+                # Get User Enrollments
+                user = enrollment.user
 
-                    print "Student with email {0} has been enrolled for {1} days and has achieved {2} percent so far".format(user.email, time_diff, percent)
+                enrolled_at = enrollment.created
 
-                    if (time_diff >= 7) and (time_diff < 14) and percent < 0.70:
-                        if not sales_email_history.motivational_email_sent:
-                            add_user_email_to_workflow('MOTIVATIONAL_EMAIL', enrollment)
-                            sales_email_history.motivational_email_sent = True
+                time_diff = (timezone.now() - enrolled_at).days
+                percent = gradeset['percent']
 
-                    elif time_diff >= 14 and percent < 0.80:
-                        if percent < 0.30:
-                            if not sales_email_history.sales_funnel_email_sent:
-                                add_user_email_to_workflow('LESS_THAN_30', enrollment)
-                                sales_email_history.sales_funnel_email_sent = True
+                print "Student with email {0} has been enrolled for {1} days and has achieved {2} percent so far".format(
+                    user.email, time_diff, percent)
 
-                        elif (percent >= 0.30) and (percent < 0.50):
-                            if not sales_email_history.promote_lynx_account_email_sent:
-                                add_user_email_to_workflow('LESS_THAN_50', enrollment)
-                                sales_email_history.promote_lynx_account_email_sent = True
+                if (time_diff >= 7) and (time_diff < 14) and percent < 0.70:
+                    if not sales_email_history.motivational_email_sent:
+                        add_user_email_to_workflow('MOTIVATIONAL_EMAIL', enrollment)
+                        sales_email_history.motivational_email_sent = True
 
-                    if percent >= 0.80:
-                        if not sales_email_history.congratulation_email_sent:
-                            add_user_email_to_workflow('MORE_THAN_80', enrollment)
-                            sales_email_history.congratulation_email_sent = True
+                elif time_diff >= 14 and percent < 0.80:
+                    if percent < 0.30:
+                        if not sales_email_history.sales_funnel_email_sent:
+                            add_user_email_to_workflow('LESS_THAN_30', enrollment)
+                            sales_email_history.sales_funnel_email_sent = True
 
-                    else:
-                        print "No conditions matched"
+                    elif (percent >= 0.30) and (percent < 0.50):
+                        if not sales_email_history.promote_lynx_account_email_sent:
+                            add_user_email_to_workflow('LESS_THAN_50', enrollment)
+                            sales_email_history.promote_lynx_account_email_sent = True
 
-                    sales_email_history.save()
+                if percent >= 0.80:
+                    if not sales_email_history.congratulation_email_sent:
+                        add_user_email_to_workflow('MORE_THAN_80', enrollment)
+                        sales_email_history.congratulation_email_sent = True
+
+                else:
+                    print "No conditions matched"
+
+                sales_email_history.save()
